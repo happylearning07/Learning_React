9b0eb8b0929758d221041f7e93c0eaa84b94e9c1
happylearning07 <juhisahni07@gmail.com>
2025-10-18 07:20:04 +0530

    Adding Notes Saver Application

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+import { createBrowserRouter, RouterProvider, useParams } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ViewPaste from './components/ViewPaste';
+import Paste from './components/Paste';
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<div>
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+    {
+      path:"/pastes",
+      element:<div>
+        <Navbar/>
+        <Paste/>
+      </div>
+    },
+    {
+      path:"/pastes/:id",
+      element:
+      <div>
+        <Navbar/>
+        <ViewPaste/>
+      </div>
+    },
+  ]
+);
+
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router = {router}/>
+    </div>
+  )
+}
+
+export default App
